265d9a22308b3ec60a6e2b2fca7fc6b2be7eacfc
Vincent Berthoux <twinside@gmail.com>
2013-12-16 21:04:46 +0100

    Got a hole in the structure :)

--- exec-src/rastertest.hs
+++ exec-src/rastertest.hs
@@ -6,8 +6,8 @@ pathize :: [V2 Float] -> [Bezier]
 pathize (a:b:rest@(c:_)) = Bezier a b c : pathize rest
 pathize _ = []
 
-circle :: Int -> V2 Float -> [Bezier]
-circle size offset = pathize $ map (^+^ offset)
+circle :: Int -> Bool -> V2 Float -> [Bezier]
+circle size inv offset = pathize . way $ map (^+^ offset)
     [ (V2   0  is)
     , (V2   0   0)
     , (V2  is   0)
@@ -21,6 +21,9 @@ circle size offset = pathize $ map (^+^ offset)
   where is = fromIntegral size
         is2 = is + is
 
+        way | inv = reverse
+            | otherwise = id
+
 circleTest :: IO ()
 circleTest = do
     mapM_ print beziers
@@ -33,9 +36,10 @@ circleTest = do
     putStrLn "========================"
     writePng "circle.png" img
   where texture = uniformTexture black
-        beziers = circle 20 $ V2 10 10
-        drawing = fillBezierShape texture beziers
-        white = (PixelRGBA8 255 255 255 255)
+        beziers = circle 40 False $ V2 10 10
+        inverse = circle 20 True $ V2 20 20
+        drawing = fillBezierShape texture $ beziers ++ inverse
+        white = (PixelRGBA8 255 255 255 0)
         black = (PixelRGBA8 0 0 16 255)
         img = renderContext 100 100 white drawing
 
